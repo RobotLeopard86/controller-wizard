49c7bceb355f2dbe7360befd927bab5022667730
RobotLeopard86 <owensiebers@icloud.com>
2024-09-12 16:29:59 -0700

    4-space tab invasion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
-  ],
-  plugins: [
-      require('flowbite/plugin')
-  ],
-  theme: {}
+	content: [
+		'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+		'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+		'./index.html',
+		'./src/**/*.{vue,js,ts,jsx,tsx}'
+	],
+	plugins: [
+			require('flowbite/plugin')
+	],
+	theme: {}
 }
 
