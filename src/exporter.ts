72afd5b54c78979e8fb6958c8076537a65e337b8
RobotLeopard86 <owensiebers@icloud.com>
2025-01-10 10:34:36 -0800

    Add some mappings for combo export

--- src/exporter.ts
+++ src/exporter.ts
@@ -9,35 +9,54 @@ export const exportProject = (inst: Instance) => {
 			mappingsAsDict.set(m.trigger, m.action);
 		});
 
+		const abbr: Map<Button, string> = new Map([
+			['A', "A"],
+			['B', "B"],
+			['X', "X"],
+			['Y', "Y"],
+			['Left Button', "LB"],
+			['Right Button', "RB"],
+			['Left Trigger', "LT"],
+			["Right Trigger", "RT"],
+			['Back', "Back"],
+			['Start', "Start"],
+			['D-Pad Up', "DPadU"],
+			['D-Pad Left', "DPadL"],
+			['D-Pad Right', "DPadR"],
+			['D-Pad Down', "DPadD"],
+			['Left Stick X', "LStk-X"],
+			['Left Stick Y', "LStk-X"],
+			['Left Stick Press', "LStkClk"],
+			['Right Stick X', "RStk-X"],
+			['Right Stick Y', "RStk-X"],
+			['Right Stick Press', "RStkClk"]
+		]);
+
+		const initialPos: Map<Button, {x: number, y: number}> = new Map([
+			['Left Trigger', {x: 10, y: 11}],
+			['Left Button', {x: 10, y: 119}],
+			['Left Stick X', {x: 10, y: 227}],
+			['Left Stick Y', {x: 10, y: 335}],
+			['Left Stick Press', {x: 10, y: 443}],
+			['Back', {x: 10, y: 551}],
+			['D-Pad Up', {x: 10, y: 659}],
+			['D-Pad Down', {x: 10, y: 767}],
+			['D-Pad Left', {x: 10, y: 875}],
+			['D-Pad Right', {x: 10, y: 983}],
+			['Right Trigger', {x: 1240, y: 11}],
+			['Right Button', {x: 1240, y: 119}],
+			['Y', {x: 1240, y: 227}],
+			['X', {x: 1240, y: 335}],
+			['B', {x: 1240, y: 443}],
+			['A', {x: 1240, y: 551}],
+			['Start', {x: 1240, y: 659}],
+			['Right Stick X', {x: 1240, y: 767}],
+			['Right Stick Y', {x: 1240, y: 875}],
+			['Right Stick Press', {x: 1240, y: 983}],
+		]);
+
 		const canvas = new OffscreenCanvas(1920, 1080);
 		const ctx = canvas.getContext('2d')!;
-
-		const distanceFromCenter = 165;
-		const LEFT = canvas.width / 2 - distanceFromCenter;
-		const RIGHT = canvas.width / 2 + distanceFromCenter - 6;
-
-		const buttonPositions = {
-			"Back": [LEFT, 17],
-			"Start": [RIGHT, 17],
-			"Left Button": [LEFT, 55],
-			"Right Button": [RIGHT, 53],
-			"Left Trigger": [LEFT, 89],
-			"Right Trigger": [RIGHT, 80],
-			"Left Stick Y": [LEFT, 128],
-			"Left Stick X": [LEFT, 156],
-			"Left Stick Press": [LEFT, 186],
-			"Y": [RIGHT, 114],
-			"B": [RIGHT, 160],
-			"X": [RIGHT, 182],
-			"A": [RIGHT, 273],
-			"D-Pad Up": [LEFT, 292],
-			"D-Pad Left": [LEFT, 320],
-			"D-Pad Down": [LEFT, 347],
-			"D-Pad Right": [LEFT, 378],
-			"Right Stick Y": [RIGHT, 320],
-			"Right Stick X": [RIGHT, 347],
-			"Right Stick Press": [RIGHT, 376],
-		};
 
 		async function drawController() {
 			let controller = new Image();
@@ -45,26 +64,9 @@ export const exportProject = (inst: Instance) => {
 			await new Promise((resolve) => controller.addEventListener('load', resolve));
 
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-
 			ctx.fillStyle = '#FFFFFF';
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
 			ctx.drawImage(controller, 0, 0, canvas.width - 6, 406);
-
-			ctx.textBaseline = 'middle';
-			ctx.direction = 'ltr';
-			ctx.font = '32px Arial';
-			ctx.fillStyle = '#000000';
-			ctx.fillText(scheme.name, 5, canvas.height - 18);
-
-			for(const [trigger, action] of mappingsAsDict) {
-				const position = buttonPositions[trigger];
-				if (position && action) {
-					ctx.fillStyle = '#000';
-					ctx.font = '16px Arial';
-					ctx.direction = position[0] === LEFT ? 'rtl' : 'ltr';
-					ctx.fillText(action, position[0], position[1] + 16 / 2.8);
-				}
-			}
 
 			requestAnimationFrame(drawController);
 		}
